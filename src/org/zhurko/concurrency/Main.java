bca1290398c9b1bbdd00169764eedf1dff1d841d
igor <zhurkoigor@gmail.com>
2022-08-27 20:00:46 +0300

    Formatting performed.

--- src/org/zhurko/concurrency/Main.java
+++ src/org/zhurko/concurrency/Main.java
@@ -5,11 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Foo foo = new Foo();
 
-        for (int i = 0; i < 1000; i++) {
-            new FooThreadC(foo, "C");
-            new FooThreadB(foo, "B");
-            new FooThreadA(foo, "A");
-            System.out.println();
-        }
+        new FooThreadC(foo, "C");
+        new FooThreadB(foo, "B");
+        new FooThreadA(foo, "A");
     }
 }
